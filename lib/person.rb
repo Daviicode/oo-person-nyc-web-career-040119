5f62e4701935a0dbe46bc5aff4a18426d4fb07c3
David zhang <d.ydzdavid@yahoo.com>
2019-04-03 21:40:38 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,21 @@
 # your code goes here
+class Person
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def bank_account 
+    @bank_account = 25
+  end 
+  
+  def happiness_points 
+    @happiness_points = 8
+  end
+  
+  def hygiene_point 
+    @hygiene_point = 8
+  end
+    
+end
